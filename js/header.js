b935571546b8fdcfd71453b5d0340bec4b404cc9
Antoine Pavy <antoine.pavy92@gmail.com>
2022-07-04 00:42:41 +0200

    Fixed structural issues + siggraph done

--- js/header.js
+++ js/header.js
@@ -34,31 +34,17 @@ class MyHeader extends HTMLElement {
                                 <li class="dropdown megamenu-fw">
                                     <a href="/index.html">Home</a><i class="fas fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
                                 </li>
-                                <!-- end menu item -->
                                 <li class="dropdown megamenu-fw">
-                                    <a href="/portfolio/portfolio.html">Portfolio</a><i class="fas fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
-                                </li>
-                                <li class="dropdown megamenu-fw">
-                                    <a href="#">Contactez-moi</a><i class="fas fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
+                                    <a href="mailto:antoine.pavy92@gmail.com">Contactez-moi</a><i class="fas fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
                                 </li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-auto pe-0">
-                        <div class="header-searchbar">
-                            <a href="#search-header" class="header-search-form"><i class="fas fa-search search-button"></i></a>
-                            <!-- search input-->
-                            <form id="search-header" method="post" action="search-result.html" name="search-header" class="mfp-hide search-form-result">
-                                <div class="search-form position-relative">
-                                    <button type="submit" class="fas fa-search close-search search-button"></button>
-                                    <input type="text" name="search" class="search-input" placeholder="Enter your keywords..." autocomplete="off">
-                                </div>
-                            </form>
-                        </div>
                         <div class="header-social-icon d-none d-md-inline-block">
-                            <a href="https://www.facebook.com/" title="Facebook" target="_blank"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
-                            <a href="https://twitter.com/" title="Twitter" target="_blank"><i class="fab fa-twitter"></i></a>
-                            <a href="https://dribbble.com/" title="Dribbble" target="_blank"><i class="fab fa-dribbble"></i></a>                          
+                            <a class="linkedin" href="https://www.linkedin.com/in/antoine-pavy/" target="_blank"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
+                            <a class="github" href="https://github.com/Loutrinator" target="_blank"><i class="fab fa-github" aria-hidden="true"></i></a>
+                            <a class="instagram" href="https://www.instagram.com/the.3dee.guy/" target="_blank"><i class="fab fa-instagram me-0" aria-hidden="true"></i></a>                          
                         </div>
                     </div>
                 </div>
